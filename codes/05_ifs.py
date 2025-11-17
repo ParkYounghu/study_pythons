6af0b1bc4f16665af9a3f691da3b64dbe59bc2f9
ParkYounghu <dudgn7715@gmail.com>
2025-11-17 18:03:13 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,16 @@
+first = 6
+
+# if first <= 5 :
+#     print("welcome to if statements")
+#     print("It's me!")
+
+second = 7
+
+first = int(input("input first number: "))
+second = int(input("input second number: "))
+if first <= second :
+    print("welcome to if statements")
+    print("It's me!")
+
+
+print("end of program")
