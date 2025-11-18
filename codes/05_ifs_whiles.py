54dfc59540e4fea0c93558f0c708727fef10da7f
ParkYounghu <dudgn7715@gmail.com>
2025-11-18 14:59:36 +0900

    toy

--- codes/05_ifs_whiles.py
+++ codes/05_ifs_whiles.py
@@ -19,7 +19,7 @@ first = 5
 
 while first > 0:
     print(f"while 값 : {first}")
-    if first == 3:     # 실제로는 first == 3 에서 break해야 하는 문제 조건, < 지우고 == 추가
+    if first == 3:     # 실제로는 first == 3 에서 break해야 하는 문제 조건, < 지우고 == 추가 혹은 = 추가해서 <= 로 변경
         print("break 실행")
         break
     first = first - 1
