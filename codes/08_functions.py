ac42c978ec503b4133f89946959917ce83cd1b49
ParkYounghu <dudgn7715@gmail.com>
2025-11-19 13:19:36 +0900

    toy

---
+++ codes/08_functions.py
@@ -0,0 +1,35 @@
+# 함수 실습 위한  기본 코드
+
+# temp = 77
+# celsius1 = (temp - 32) * 5 / 9
+# print(celsius1)
+
+# temp = 95
+# celsius = (temp - 32) * 5 / 9
+# print(celsius)
+
+# temp = 50
+# celsius = (temp - 32) * 5 / 9
+# print(celsius)
+
+# 함수 사용
+
+# def function_name(param_first, ..., param_last):
+#      실행할 코드
+#      return return_value
+
+# def to_celsius(temp): # 함수의 이름 정의
+#     celsius = (temp - 32) * 5 / 9 # 함수 공식
+#     return celsius # 함수의 결과값을 함수의 이름에게 전해준다.
+
+# pass
+# print(to_celsius(120)) # temp가 120일 때 함수의 값 출력
+# temp1 = to_celsius(77) # temp1은 temp가 77일 때의 함수값과 같다.
+# print(temp1)           # 위에서 구한 temp1 값 출력   
+# pass
+
+
+# temp2 = 100
+# result2 = to_celsius(temp2)
+# print(result2)
+# pass
